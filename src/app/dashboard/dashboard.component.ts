5434f3b35166f182b58554046d323f5a9256bdef
Henrietha <areathy33@gmail.com>
2021-08-26 10:20:59 +0100

    Dashboard set up

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,13 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit
+{
+  Designation: string = "";
+  Username: string = "";
+  NoOfTeamMembers: number = 0;
+  TotalCostOfAllProjects: number = 0;
+  PendingTasks: number = 0;
+  UpComingProjects: number = 0;
+  ProjectCost: number = 0;
+  CurrentExpenditure: number = 0;
+  AvailableFunds: number = 0;
 
-  constructor() { }
+  Clients: string[] = [];
+  Projects: string[] = [];
+  Years: number[] = [];
+  TeamMembersSummary: any = [];
+  TeamMembers: any = [];
 
-  ngOnInit(): void {
+  ngOnInit()
+  {
+    this.Designation = 'Team Leader';
+    this.Username = 'Scott Smith';
+    this.NoOfTeamMembers = 67;
+    this.TotalCostOfAllProjects = 240;
+    this.PendingTasks = 15;
+    this.UpComingProjects = 2;
+    this.ProjectCost = 2113507;
+    this.CurrentExpenditure = 96788;
+    this.AvailableFunds = 52536;
   }
-
 }
