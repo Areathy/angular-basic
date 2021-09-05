e9357c2ba6f4b816d03237400949e02be1d48924
Henrietha <areathy33@gmail.com>
2021-09-05 23:21:25 +0100

    Cross Site Scripting (XSS)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { LoginService } from './login.service';
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
@@ -8,9 +10,13 @@ import { LoginService } from './login.service';
 })
 export class AppComponent
 {
-  constructor(public loginService: LoginService)
-  {
-  }
+  constructor(public loginService: LoginService, private domSanitizer: DomSanitizer) {}
+
+  // myProperty = "<svg>Henrietha<svg>";
+  // myProperty = "<script>alert(document.cookie)</script>";
+  // myProperty = this.domSanitizer.bypassSecurityTrustHtml("<iframe src='http://www.lipsum.com'></iframe>");
+  // myProperty = this.domSanitizer.bypassSecurityTrustUrl("javascript:window.open('http://www.google.com')");
+  myProperty = this.domSanitizer.bypassSecurityTrustResourceUrl("https://upload.wikimedia.org/wikipedia/commons/1/12/User_icon_2.svg");
 
   onSearchClick()
   {
