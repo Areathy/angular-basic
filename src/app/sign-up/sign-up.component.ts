b2026346c901102b702fa671fbfae214dfc5cf82
Henrietha <areathy33@gmail.com>
2021-09-07 12:41:27 +0100

    Radio buttons in reactive forms

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -19,7 +19,8 @@ export class SignUpComponent implements OnInit
       lastName: new FormControl(null),
       email: new FormControl(null),
       mobile: new FormControl(null),
-      dateOfBirth: new FormControl(null)
+      dateOfBirth: new FormControl(null),
+      gender: new FormControl(null); 
     });
   }
 }
