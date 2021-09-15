2bbb37f698b27a613a9ba74c09240de99e0fd38c
Henrietha <areathy33@gmail.com>
2021-09-15 18:37:58 +0100

    HostBinding

--- src/app/alert.directive.ts
+++ src/app/alert.directive.ts
@@ -1,20 +1,24 @@
-import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appAlert]'
 })
 export class AlertDirective {
   @Input("error") error!: string;
+  @HostBinding("title") title!: string;
   
   constructor(private elementRef: ElementRef) {
   }
 
   ngOnInit() {
     this.elementRef.nativeElement.innerHTML = `
-      <div class="alert alert-danger fade show" role="alert" style="transition: transform 0.5s">
+      <div class="alert alert-danger fade show" role="alert" style="transition: transform 0.2s">
         <span>${this.error}</span>
       </div>
     `;
+
+    this.title = "Please try again!";
+
   }
 
   @HostListener("mouseenter", [ "$event" ])
