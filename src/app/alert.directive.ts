f74d2c9de6afeec1eb87bb66f19e2dd1973f1916
Henrietha <areathy33@gmail.com>
2021-09-15 20:22:17 +0100

    Renderer2

--- src/app/alert.directive.ts
+++ src/app/alert.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostBinding, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appAlert]'
@@ -7,15 +7,40 @@ export class AlertDirective {
   @Input("error") error!: string;
   @HostBinding("title") title!: string;
   
-  constructor(private elementRef: ElementRef) {
+  constructor(private elementRef: ElementRef, private renderer2 : Renderer2) {
   }
 
+  divElement: any;
+  spanElement: any;
+  spanText: any;
+
   ngOnInit() {
-    this.elementRef.nativeElement.innerHTML = `
-      <div class="alert alert-danger fade show" role="alert" style="transition: transform 0.2s">
-        <span>${this.error}</span>
-      </div>
-    `;
+
+    /* div */
+    this.divElement = this.renderer2.createElement("div"); //<div></div>
+
+    this.renderer2.setAttribute(this.divElement, "role", "alert"); //<div role="alert"> </div>
+
+    this.renderer2.setAttribute(this.divElement, "class", "alert alert-danger fade show");
+    //<div role="alert" class="alert alert-danger fade show"> </div>
+
+    this.renderer2.setStyle(this.divElement, "transition", "transform 0.5s");
+    //<div role="alert" class="alert alert-danger fade show" style="transition: transform 0.5s"> </div>
+
+    /* span */
+    this.spanElement = this.renderer2.createElement("span");
+    this.renderer2.setAttribute(this.spanElement, "class", "message");
+    //<span class="message"></span>
+
+    /* spanText */
+    this.spanText = this.renderer2.createText(this.error);
+    this.renderer2.appendChild(this.spanElement, this.spanText);
+    //<span class="message">${this.error}</span>
+
+    this.renderer2.appendChild(this.divElement, this.spanElement);
+    //<div role="alert" class="alert alert-danger fade show" style="transition: transform 0.5s"> <span class="message">${this.error}</span> </div>
+
+    this.elementRef.nativeElement.appendChild(this.divElement);
 
     this.title = "Please try again!";
 
@@ -23,12 +48,14 @@ export class AlertDirective {
 
   @HostListener("mouseenter", [ "$event" ])
   onMouseEnter(event: any) {
-    this.elementRef.nativeElement.querySelector(".alert").style.transform = "scale(1.1)";
+    // this.elementRef.nativeElement.querySelector(".alert").style.transform = "scale(1.1)";
+    this.renderer2.setStyle(this.divElement, "transform", "scale(1.1)");
   }
 
   @HostListener("mouseleave", [ "$event" ])
   onMouseLeave() {
-    this.elementRef.nativeElement.querySelector(".alert").style.transform = "scale(1)";
+    // this.elementRef.nativeElement.querySelector(".alert").style.transform = "scale(1)";
+    this.renderer2.setStyle(this.divElement, "transform", "scale(1)");
   }
 
 }
