36f6d60b67925a081e122eafa635e0a2771a2d91
Henrietha <areathy33@gmail.com>
2021-09-20 00:14:01 +0100

    Shared Module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,62 +1,27 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { MyProfileComponent } from './components/my-profile/my-profile.component';
-import { AboutComponent } from './about/about.component';
-import { DashboardService } from '../services/dashboard.service';
-import { ProjectsComponent } from './components/projects/projects.component';
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { MyProfileComponent } from "./components/my-profile/my-profile.component";
+import { DashboardService } from "../services/dashboard.service";
+import { ProjectsComponent } from "./components/projects/projects.component";
 import { ProjectComponent } from './components/project/project.component';
-import { FormsModule } from '@angular/forms';
-import { TeamSizeValidatorDirective } from '../directives/team-size-validator.directive';
-import { ClientLocationStatusValidatorDirective } from '../directives/client-location-status-validator.directive';
-import { ProjectIDUniqueValidatorDirective } from '../directives/project-idunique-validator.directive';
 import { CheckBoxPrinterComponent } from './components/check-box-printer/check-box-printer.component';
-import { NumberToWordsPipe } from '../pipes/number-to-words.pipe';
-import { FilterPipe } from '../pipes/filter.pipe';
-import { PagingPipe } from '../pipes/paging.pipe';
 import { ProjectsDetailsComponent } from './components/projects-details/projects-details.component';
 import { AdminRoutingModule } from './admin-routing/admin-routing.module';
-import { EmployeeRoutingModule } from '../employee/employee-routing/employee-routing.module';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     MyProfileComponent,
-    AboutComponent,
     ProjectsComponent,
-    TeamSizeValidatorDirective,
-    ClientLocationStatusValidatorDirective,
-    ProjectIDUniqueValidatorDirective,
     ProjectComponent,
     CheckBoxPrinterComponent,
-    NumberToWordsPipe,
-    FilterPipe,
-    PagingPipe,
-    ProjectsDetailsComponent
+    ProjectsDetailsComponent 
   ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    AdminRoutingModule
-  ],
-  exports: [
-    DashboardComponent,
-    MyProfileComponent,
-    AboutComponent,
-    ProjectsComponent,
-    TeamSizeValidatorDirective,
-    ClientLocationStatusValidatorDirective,
-    ProjectIDUniqueValidatorDirective,
-    ProjectComponent,
-    CheckBoxPrinterComponent,
-    NumberToWordsPipe,
-    FilterPipe,
-    PagingPipe,
-    ProjectsDetailsComponent
-  ],
-  providers: [
-    DashboardService
-  ]
+  imports: [ SharedModule, AdminRoutingModule ],
+  exports: [ DashboardComponent, MyProfileComponent, ProjectsComponent, ProjectsDetailsComponent ],
+  providers: [ DashboardService ]
 })
-export class AdminModule { }
+export class AdminModule
+{
+}
--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -1,22 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { TasksComponent } from './components/tasks/tasks.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EmployeeRoutingModule } from './employee-routing/employee-routing.module';
-
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
-    TasksComponent,
+    TasksComponent
   ],
   imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
+    SharedModule,
     EmployeeRoutingModule
   ],
-  exports: [
+  exports:
+  [
     TasksComponent
   ]
 })
