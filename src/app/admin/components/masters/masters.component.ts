f303362995ceb1debc781bedb892f48e55a53aa5
Henrietha <areathy33@gmail.com>
2021-09-20 23:18:45 +0100

    Prepare Masters Menu Dynamically

--- src/app/admin/components/masters/masters.component.ts
+++ src/app/admin/components/masters/masters.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientLocationsComponent } from '../client-locations/client-locations.component';
+import { CountriesComponent } from '../countries/countries.component';
+import { TaskPrioritiesComponent } from '../task-priorities/task-priorities.component';
+import { TaskStatusComponent } from '../task-status/task-status.component';
 
 @Component({
   selector: 'app-masters',
@@ -6,6 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./masters.component.scss']
 })
 export class MastersComponent implements OnInit {
+
+  masterMenuItems = [
+    { itemName: "Countries", displayName: "Countries", component: CountriesComponent },
+    { itemName: "ClientLocations", displayName: "Client Locations", component: ClientLocationsComponent },
+    { itemName: "TaskPriorities", displayName: "Task Priorities", component: TaskPrioritiesComponent },
+    { itemName: "TaskStatus", displayName: "Task Status", component: TaskStatusComponent },
+  ];
+
+  activeItem!: string;
+  tabs = [];
 
   constructor() { }
 
