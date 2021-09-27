f2964d1b1aaef3ceb2f928c3b71208b5e0108ea8
Henrietha <areathy33@gmail.com>
2021-09-27 23:18:59 +0100

    Initial CRUD commit for task priority and task status

---
+++ src/app/models/grouped-task.ts
@@ -0,0 +1,10 @@
+export class GroupedTask {
+
+    taskStatusName: number;
+    tasks: Task[];
+
+    constructor() {
+        this.taskStatusName = null!;
+        this.tasks = null!;
+    }
+}
---
+++ src/app/models/task-status.ts
@@ -0,0 +1,10 @@
+export class TaskStatus {
+    taskStatusID: number;
+    taskStatusName: string;
+
+    constructor()
+    {
+        this.taskStatusID = null!;
+        this.taskStatusName = null!;
+    }
+}
